3137b671aa8f9a875600381a68145ce116397af2
siddharth7actowiz <siddharth7.actowiz@gmail.com>
2026-03-03 09:43:56 +0530

    Update mian_controller.py

--- mian_controller.py
+++ mian_controller.py
@@ -1,13 +1,15 @@
 from load_parse import load_json,parser
 from store_to_database import insert_into_database
+from config import ZIP_PATH
 
-zip_path=r"D:\Siddharth\Grabfood_unzip_load\grab_food_pages.zip"
+
 def main():
-    json_data = load_json(zip_path)
+    json_data = load_json(ZIP_PATH)
     validated_data = parser(json_data)
     insert_into_database(validated_data)
 
-
+# Main Function Call
 main()
+
 
 
